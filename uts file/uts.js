92237d9e1ffb8032d4b8d0948671be6399128df9
betonk26 <betonk26@gmail.com>
2022-10-08 00:37:44 +0700

    first impact

--- uts file/uts.js
+++ uts file/uts.js
@@ -1,1 +1,36 @@
-let dataArr =   [];
+var array = [];
+
+fetch("./uts.json")
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        array = data.tani // tani data dari JSON
+        dataFix(); // membuat function baru
+    })
+    .catch(function (err){
+        return false;
+    })
+
+function dataFix(){
+    let tbl     = document.getElementById('tableku')
+    let tbody   = document.createElement('tbody')
+    for(i = 0; i<array.length; i++){
+        let kolom = document.createElement('tr')
+        for(let a in array[i]){
+            let isi = document.createElement('td')
+            let item = document.createTextNode(array[i][a])
+            isi.appendChild(item)
+            kolom.appendChild(isi)
+        }
+        tbody.appendChild(kolom)
+    }
+    tbl.appendChild(tbody)
+}
+
+function showData(){
+    document.getElementById('tableku').style.display = "table"
+}
+function closeData(){
+    document.getElementById('tableku').style.display = "none"
+}
